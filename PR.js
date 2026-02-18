64ccc5225ba93c2bd0cf50276011c2b12ff0ada6
L-port8000 <sub.sasaki2000@outlook.jp>
2026-02-18 12:56:12 +0900

    Create PR.js

---
+++ PR.js
@@ -0,0 +1,17 @@
+const adData = [
+    {
+        url: "https://blog.soyaakinohara.net/2026/02/04/オリキャラをvtuberにして24時間耐久配信させよう！！",
+        title: "オリキャラをvtuberにして24時間耐久配信させよ...",
+        favicon: "https://www.google.com/s2/favicons?domain=blog.soyaakinohara.net&sz=64"
+    },
+    {
+        url: "https://youtu.be/-Tt-lH7KNJw?si=BiW-RBdlaM78IeIC",
+        title: "【激安】ゲオで売っているSSDを買ってみた。",
+        favicon: "https://pbs.twimg.com/profile_images/2017818838545666050/R5GmLmhJ_400x400.jpg"
+    },
+    {
+        url: "https://miuraakihito82.blogspot.com/2026/02/blog-post_11.html",
+        title: "【娯楽の革命】今こそラジオを聴こう！",
+        favicon: "https://www.google.com/s2/favicons?domain=miuraakihito82.blogspot.com&sz=64"
+    }
+];
