2ae55563af365c25ec555e29443a52688ebc0fbe
一樹（いつき） <sub.sasaki2000@outlook.jp>
2026-05-03 09:12:48 +0900

    None

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const i18nData = {
     ja: {
-        nav_home: "ホーム", nav_about: "私について", nav_projects: "プロジェクト", nav_contact: "連絡先", nav_etc: "その他",
+        nav_home: "ホーム", nav_about: "私について", nav_projects: "プロジェクト", nav_blog: "ブログ", nav_contact: "連絡先", nav_etc: "その他",
         settings_title: "設定", mode_label: "ダークモード", lang_label: "言語",
         home_text: "ようこそ。こちらは一樹の個人ホームページです。\n特に大したものは有りませんが、ゆっくり見ていってください！",
         news: "おすすめ",
@@ -12,16 +12,16 @@ const i18nData = {
         title_blank: "募集中",
         archive_title: "過去ホームページアーカイブ",
         archive_content: "私の過去のホームページの一覧です。メインのindex.htmlのみ保存しています。",
-        archive_button: "アーカイブページへ", 
-        
+        archive_button: "アーカイブページへ",
         about_hujin: "布陣",
         blog_1: "アカウントが乗っ取られた話",
         blog_2: "アカウントが復元できた話",
         blog_3: "",
-        blog_4: ""
+        blog_4: "",
+        backtohome: "←ホームに戻る"
     },
     en: {
-        nav_home: "Home", nav_about: "About", nav_projects: "Projects", nav_contact: "Contact", nav_etc: "Other",
+        nav_home: "Home", nav_about: "About", nav_projects: "Projects", nav_blog: "blog", nav_contact: "Contact", nav_etc: "Other",
         settings_title: "Settings", mode_label: "Dark Mode", lang_label: "Language",
         home_text: "Welcome.",
         news: "Recommended",
@@ -32,10 +32,16 @@ const i18nData = {
         title_blank: "Open",
         archive_title: "Archive of Past Websites",
         archive_content: "This is a list of my past websites. Only the main index.html files are preserved.",
-        archive_button: "Go to Archive Page"
+        archive_button: "Go to Archive Page",
+        about_hujin: "",
+        blog_1: "",
+        blog_2: "",
+        blog_3: "",
+        blog_4: "",
+        backtohome: "←back to home"
     },
     "zh-CN": {
-        nav_home: "首页", nav_about: "关于", nav_projects: "项目", nav_contact: "联系", nav_etc: "其他",
+        nav_home: "首页", nav_about: "关于", nav_projects: "项目", nav_blog: "()()()", nav_contact: "联系", nav_etc: "其他",
         settings_title: "设置", mode_label: "深色模式", lang_label: "语言",
         home_text: "欢迎。",
         news: "推荐",
@@ -46,10 +52,16 @@ const i18nData = {
         title_blank: "招募中",
         archive_title: "过往网站存档",
         archive_content: "这是我过去制作的网站列表，仅保存了主页面（index.html）。",
-        archive_button: "前往存档页面"
+        archive_button: "前往存档页面",
+        about_hujin: "",
+        blog_1: "",
+        blog_2: "",
+        blog_3: "",
+        blog_4: "",
+        backtohome: ""
     },
     "zh-TW": {
-        nav_home: "首頁", nav_about: "關於", nav_projects: "項目", nav_contact: "聯繫", nav_etc: "其他",
+        nav_home: "首頁", nav_about: "關於", nav_projects: "項目", nav_blog: "()()()", nav_contact: "聯繫", nav_etc: "其他",
         settings_title: "設置", mode_label: "深色模式", lang_label: "語言",
         home_text: "歡迎。",
         news: "推薦",
@@ -60,7 +72,13 @@ const i18nData = {
         title_blank: "招募中",
         archive_title: "過去網站存檔",
         archive_content: "這是我過去製作的網站列表，僅保留主頁（index.html）。",
-        archive_button: "前往存檔頁面"
+        archive_button: "前往存檔頁面",
+        about_hujin: "",
+        blog_1: "",
+        blog_2: "",
+        blog_3: "",
+        blog_4: "",
+        backtohome: ""
     }
 };
 
